193a8a762cb77170bd2a279bbe8cf6881f8034a4
Loremaiden <loremaiden@gmail.com>
2023-03-26 19:07:25 -0300

    quedo solo el back

---
+++ src/main/java/com/portfolio/lorenab/Dto/dtoEducacion.java
@@ -0,0 +1,45 @@
+
+package com.portfolio.lorenab.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ *
+ * @author Lorena Borrego
+ */
+public class dtoEducacion {
+     @NotBlank
+    private String nombreE;
+    @NotBlank
+    private String descripcionE;
+    
+    //const
+
+    public dtoEducacion() {
+    }
+
+    public dtoEducacion(String nombreE, String descripcionE) {
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+    }
+    
+    
+//gys
+    public String getNombreE() {
+        return nombreE;
+    }
+
+    public void setNombreE(String nombreE) {
+        this.nombreE = nombreE;
+    }
+
+    public String getDescripcionE() {
+        return descripcionE;
+    }
+
+    public void setDescripcionE(String descripcionE) {
+        this.descripcionE = descripcionE;
+    }
+    
+    
+}
